921e0a25505ac50dc9c9ec2d30538366a90eb0a7
jrdnrs <55672049+jrdnrs@users.noreply.github.com>
2024-07-05 16:24:44 +0100

    Update exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
 mod archetype;
 mod component;
 mod entity;
+mod event;
 mod query;
 mod resource;
 mod system;
-mod world;
-mod event;
 mod util;
+mod world;
 
+pub use component::Component;
 pub use entity::Entity;
+pub use event::Events;
 pub use query::bundle::{ComponentBundle, ResourceBundle};
 pub use query::filter::{And, Not, Tracked};
 pub use query::{Query, QueryBuilder};
+pub use query::iter::ComponentBundleIter;
 pub use resource::{Resource, ResourceId};
 pub use system::schedule::{Schedule, ScheduleBuilder};
 pub use system::{System, SystemFn};
-pub use component::Component;
 pub use world::*;
